958abc12ade4f4802afde7f64549ea3a6837cbe2
lippertd88 <lippertd88@gmail.com>
2023-02-16 20:31:33 +0200

    chapter 6 solutions

--- chapter_6.py
+++ chapter_6.py
@@ -9,3 +9,44 @@ while itevar < len(fruit):
     reverse_fruit = fruit[::-1]
     print(reverse_fruit[itevar])
     itevar = itevar + 1
+
+"""
+Exercise 3: Encapsulate this code in a function named count, 
+and generalize it so that it accepts the string and the letter as arguments.
+"""
+def count(word, letter):
+    """
+    Count the number of times the parameter called "letter" occurs in the parameter "word".
+    """
+    letter_count = 0
+    for itevar in word:
+        if itevar == letter:
+            letter_count = letter_count + 1
+    print('The letter "'
+    , letter
+    , '" occurs a total of '
+    , letter_count
+    , ' times in the word '
+    ,word
+    ,'.'
+    , sep='')
+
+
+"""
+Exercise 5: Take the following Python code that stores a string:
+
+str = 'X-DSPAM-Confidence:0.8475'
+
+Use find and string slicing to extract the portion of the string after the 
+colon character and then use the float function to convert the extracted string 
+into a floating point number.
+"""
+
+str = 'X-DSPAM-Confidence:0.8475'
+colon_idx = str.find(':')
+num = float(str[colon_idx + 1:])
+print(type(num))
+
+"""
+<class 'float'>
+"""
